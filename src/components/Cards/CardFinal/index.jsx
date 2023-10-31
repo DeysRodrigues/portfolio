0bb490acd78e8901d3b2c3d7f8b4fe32e1453a17
Deys Rodrigues <deysprogrammer@gmail.com>
2023-10-31 19:12:13 -0300

    add cardfinal

--- src/components/Cards/CardFinal/index.jsx
+++ src/components/Cards/CardFinal/index.jsx
@@ -1,16 +1,26 @@
 import styles from "./CardFinal.module.css"
 import pdf from "/assets/simple-deysrodrigues.pdf"
-
+import planet from "/src/assets/icons/download.svg"
 const CardFinal = ({ stylesTheme }) => {
 
+    const stylesButton = {
+        border:`${stylesTheme.border}`,
+        color: `${stylesTheme.color}`
+    }
     return (
         <div className={styles.cardFinal} style={stylesTheme}>
 
             <a href={pdf}download="seu_arquivo.pdf">
 
-                <button>
-                    baixe meu curriculo em pdf
+                <button className={styles.btnDownload} style={stylesButton}>
+                 BAIXAR PDF
+                    <img src={planet} alt="sss " />
                 </button>
+                <button className={styles.btnDownload} style={stylesButton}>
+                 BAIXAR PDF
+                    <img src={planet} alt="sss " />
+                </button>
+           
             </a>
 
         </div>
