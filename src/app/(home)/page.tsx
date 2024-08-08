4e1e743b7cd729a417821369cfdc17af37f34ead
deysrodrigues <deysprogrammer@gmail.com>
2024-08-08 16:08:28 -0300

    refactor: update import path for page.tsx

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import CardName from "../ui/cardName";
+import CardPhoto from "../ui/cardPhoto";
+import CardDescription from "../ui/cardDescription";
+import { CircleChevronDown } from "lucide-react";
+import CardProjects from "../ui/cardProjects";
+import CardTasks from "../ui/cardTasks";
+import CardTrajectory from "../ui/cardTrajectory"
+
+
+export default function Home() {
+  return (
+    <main className="flex gap-10 items-center flex-col m-5">
+      <div className="flex flex-col gap-6 justify-center items-center h-screen">
+        <section className="flex justify-center flex-wrap gap-4">
+          <CardPhoto />
+          <CardName />
+          <CardDescription />
+
+
+        </section>
+        <div className="icon-arrow w-20 h-20 mt-5"></div>
+      </div>
+      <section className="flex flex-col justify-between gap-6 w-[70%] md:flex-row">
+
+        <CardTasks></CardTasks>
+        <CardTrajectory></CardTrajectory>
+      </section>
+      <section className="w-full  md:w-[70%]">
+        <div className="">
+          <CardProjects></CardProjects>
+        </div>
+
+      </section>
+
+    </main>
+  );
+}
