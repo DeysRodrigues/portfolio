38636c06e2ba14443812d88773af674bddd6091d
deysrodrigues <deysprogrammer@gmail.com>
2024-08-22 10:56:07 -0300

    style: change mt and w

--- src/app/ui/cardTrajectory.tsx
+++ src/app/ui/cardTrajectory.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const CardTrajectory = () => {
     return (
-        <div className=" flex flex-col w-[50%]">
+        <div className=" flex flex-col w-full w-[50%]">
 
             <div className="flex flex-col gap-6 md:flex-row">
                 <div className="flex gap-6">
@@ -14,13 +15,13 @@ const CardTrajectory = () => {
 
                     </div>
                     <div className="flex flex-col px-2">
-                        <div>
+                        <div className="mt-2">
                             <span className="text-primary-text">2018</span>
                             <p className="text-secondary text-sm w-full">High school completion</p>
                             <p className="text-primary-text text-xs  w-40">CEAT - Centro de educação em tempo integral Antonio Freitas</p>
                         </div>
 
-                        <div className="mt-6">
+                        <div className="mt-3">
                             <span className="text-primary-text">2019</span>
                             <p className="text-secondary text-sm w-full">Higher education course in Physics</p>
                             <p className="text-primary-text text-xs  w-40">UFPI - Universidade Federal do Piauí</p>
@@ -36,10 +37,13 @@ const CardTrajectory = () => {
                 </div>
 
                 <div className="">
+                    <Link href="/curriculo-deys.pdf"  target="_blank" rel="noopener noreferrer">
                     <Button className="relative p-7 text-tertiary font-bold">
                         CURRÍCULO PDF
                         <span className="absolute -right-3 -top-2 bottom-2 border-2 w-full rounded-md "></span>
                     </Button>
+                    </Link>
+                   
                 </div>
             </div>
 
